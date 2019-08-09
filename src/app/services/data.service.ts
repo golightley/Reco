3f7b34df366bb0304aa2bc3a2daf77cbd3d85f97
Wangwei <wangwei0610_outlook.com>
2019-08-09 18:39:04 +0800

    Added uploadPicture and updated createNewRecommendation function with upload picture to firestore

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -5,8 +5,8 @@ import { Storage } from '@ionic/storage'
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import { FormBuilder, FormGroup, Validators  } from '@angular/forms'
-import { async } from '@angular/core/testing';
+import 'firebase/storage';
+import { makeUniqueId } from 'src/app/utils/common';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +18,7 @@ export class DataService {
   constructor(
     private storage: Storage,
     private loadingService: LoadingService
-    ) {
+  ) {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
         this.eventListRef = firebase
@@ -29,33 +29,66 @@ export class DataService {
 
   }
 
-  async createNewRecommendation(name:string,city:string,notes:string,location:any,googlePlaceId:any,googleTypes:any,placeWebsite:any,placePhone:any) {
-    // print the form results 
-    console.log("DATASERVICE.CreateNewRcommendations.FormGroup:")
+  async createNewRecommendation(
+      name: string, city: string, notes: string, location: any, googlePlaceId: any, googleTypes: any,
+      placeWebsite: any, placePhone: any, pictureDataUrl: any) {
+    // print the form results
+    console.log('SERVICE.CreateNewRcommendations.FormGroup:');
     console.log(location);
 
-    try{
-      await firebase.firestore().collection('recommendations').add({
-        name: name,
-        city: city,
-        notes: notes,
-        location:location,
-        gplaceId:googlePlaceId,
-        gType:googleTypes,
-        website:placeWebsite,
-        phone:placePhone,
-        user: firebase.auth().currentUser.uid,
-        timestamp: firebase.firestore.FieldValue.serverTimestamp()
-      }).then(function (docRef) {
-        console.log('DATASERVICE.createNewRecommendation:', docRef.id);
-      }).catch(function (error) {
-        console.error('DATASERVICE.createNewRecommendation.Error adding document: ', error);
-      });
-    }catch (err){
-      console.log("DATASERVICE.createNewRecommendation");
-      console.log(err);
-    }
+    const result = await this.loadingService.doFirebase(async () => {
+      return new Promise<any>(async (resolve, reject) => {
+        let pictureUrl = '';
+        if ( pictureDataUrl ) {
+          pictureUrl = await this.uploadPicture(pictureDataUrl);
+          console.log('pictureUrl:' + pictureUrl);
+        }
 
+        firebase.firestore().collection('recommendations').add({
+          name: name,
+          city: city,
+          notes: notes,
+          location: location,
+          gplaceId: googlePlaceId,
+          gType: googleTypes,
+          website: placeWebsite,
+          phone: placePhone,
+          picture: pictureUrl,
+          user: firebase.auth().currentUser.uid,
+          timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(docRef => {
+          console.log('SERVICE.createNewRecommendation:', docRef.id);
+          resolve(docRef.id);
+        }).catch(error => {
+          console.error('SERVICE.createNewRecommendation.Error adding document: ', error);
+          reject(error);
+        });
+      });
+    });
+    return result;
+  }
+
+  /**
+   * Upload picture of recommendation
+   */
+  async uploadPicture(pictureDataUrl) {
+    return new Promise<any>((resolve, reject) => {
+      const storageRef = firebase.storage().ref();
+      // Create a unique filename
+      const filename = `reco-pictures/${makeUniqueId(30)}.jpg`;
+      // Create a reference to reco-pictures
+      const imageRef = storageRef.child(filename);
+      imageRef.putString(pictureDataUrl, firebase.storage.StringFormat.DATA_URL)
+        .then((snapshot) => {
+          // successfully uploaded!
+          snapshot.ref.getDownloadURL().then( downloadURL => {
+            console.log('Uploaded Picture: url => ' + downloadURL);
+            resolve(downloadURL);
+          });
+        }).catch(error => {
+          reject(error);
+        });
+    });
   }
 
 
@@ -66,8 +99,8 @@ export class DataService {
       // firebase.firestore().collection('recommendations').where('questionId', '==', questionID).get()
       console.log('currentUser', firebase.auth().currentUser.uid);
       firebase.firestore().collection('recommendations')
-        // .where("user","==",firebase.auth().currentUser.uid)
-        .where('following', 'array-contains', firebase.auth().currentUser.uid)        // .where("user","==",users)
+        // .where('user','==',firebase.auth().currentUser.uid)
+        .where('following', 'array-contains', firebase.auth().currentUser.uid)        // .where('user','==',users)
         .get()
         .then((recs) => {
           console.log('getOtherReccos in service');
@@ -83,23 +116,23 @@ export class DataService {
 
   /**
    * Get recommendation data
-   * If getType is "all" then get all recommendation data.
-   * If getType is "mine" then get my recommendation data.
-   * If getType is "friends" then get recommendation data that registered by following users
+   * If getType is 'all' then get all recommendation data.
+   * If getType is 'mine' then get my recommendation data.
+   * If getType is 'friends' then get recommendation data that registered by following users
    */
   async getRecommendations(getType) {
     const recsArray: any[] = [];
     let query;
-    if ( getType === 'all' ) {
+    if (getType === 'all') {
       query = firebase.firestore().collection('recommendations');
 
-    } else if ( getType === 'mine' ) {
+    } else if (getType === 'mine') {
       query = firebase.firestore().collection('recommendations')
-                .where('user', '==', firebase.auth().currentUser.uid);
+        .where('user', '==', firebase.auth().currentUser.uid);
 
-    } else if ( getType === 'friends' ) {  // must change
+    } else if (getType === 'friends') {  // must change
       query = firebase.firestore().collection('recommendations')
-                .where('user', '==', firebase.auth().currentUser.uid); 
+        .where('user', '==', firebase.auth().currentUser.uid);
 
     }
     await this.loadingService.doFirebase(async() => {
@@ -108,19 +141,21 @@ export class DataService {
                   // get user name of recommendation
                   const userName = await this.getUserByID(rec.data().user);
                   rec.userName = userName;
+                  console.log('userName', userName);
                   recsArray.push(rec);
+                  console.log('recs data add');
           }));
         }).catch (error => {
             return error;
         });
     });
+    console.log('finish all recs data add');
     return recsArray;
   }
 
   async getUserByID(userId: string) {
     return new Promise<any> ((resolve, reject) => {
       firebase.firestore().collection('userProfile').doc(userId).get().then(docUser => {
-        // console.log('[Get User Info] handle = ' + docUser.data().handle);
         // if handle is exists then return handle, else return email
         let userName = docUser.data().email;
         if ( docUser.data().handle ) {
@@ -134,28 +169,28 @@ export class DataService {
     });
   }
 
-  setMyDetails(data):void{
-    this.storage.set('myDetails',data);
+  setMyDetails(data): void {
+    this.storage.set('myDetails', data);
 
   }
 
-  setWorldDetails(data):void{
-    this.storage.set('worldDetails',data);
+  setWorldDetails(data): void {
+    this.storage.set('worldDetails', data);
   }
 
-  setLocation(data):void{
-    this.storage.set('location',data);
+  setLocation(data): void {
+    this.storage.set('location', data);
   }
 
-  getMyDetails(): Promise <any> {
+  getMyDetails(): Promise<any> {
     return this.storage.get('myDetails');
   }
 
-  getWorldDetails(): Promise <any> {
+  getWorldDetails(): Promise<any> {
     return this.storage.get('worldDetails');
   }
 
-  getLocation(): Promise <any> {
+  getLocation(): Promise<any> {
     return this.storage.get('location');
   }
 
