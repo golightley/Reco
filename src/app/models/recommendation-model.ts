dd29daf9bfc6c8d50cf5eec9e4b744f57ddd503f
Wangwei <wangwei0610_outlook.com>
2019-08-09 18:36:42 +0800

    Added recommendation picture property

--- src/app/models/recommendation-model.ts
+++ src/app/models/recommendation-model.ts
@@ -7,8 +7,9 @@ export class RecommendationModel {
     lng: number;
     distance: number;
     userName: string;
+    picture: string;
 
-    constructor(id: string, name: string, city: string, notes: string, lat: number, lng: number, distance: number, userName: string ) {
+    constructor(id: string, name: string, city: string, notes: string, lat: number, lng: number, distance: number, userName: string, picture: string) {
       this.id    = id;
       this.name  = name;
       this.city  = city;
@@ -17,5 +18,6 @@ export class RecommendationModel {
       this.lng  = lng;
       this.distance = distance;
       this.userName = userName;
+      this.picture = picture;
     }
   }
