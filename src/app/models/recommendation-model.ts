d20c44f5c4afdfeba17aa9582b04e67bfc6abaac
Wangwei <wangwei0610_outlook.com>
2019-08-10 03:13:56 +0800

    Added thumbnail picture property

--- src/app/models/recommendation-model.ts
+++ src/app/models/recommendation-model.ts
@@ -1,23 +1,26 @@
 export class RecommendationModel {
-    id: string;
-    name: string;
-    city: string;
-    notes: string;
-    lat: number;
-    lng: number;
-    distance: number;
-    userName: string;
-    picture: string;
+  id: string;
+  name: string;
+  city: string;
+  notes: string;
+  lat: number;
+  lng: number;
+  distance: number;
+  userName: string;
+  picture: string;
+  pictureThumb: string;
 
-    constructor(id: string, name: string, city: string, notes: string, lat: number, lng: number, distance: number, userName: string, picture: string) {
-      this.id    = id;
-      this.name  = name;
-      this.city  = city;
-      this.notes = notes;
-      this.lat   = lat;
-      this.lng  = lng;
-      this.distance = distance;
-      this.userName = userName;
-      this.picture = picture;
-    }
+  constructor(id: string, name: string, city: string, notes: string, lat: number,
+    lng: number, distance: number, userName: string, picture: string, pictureThumb: string) {
+    this.id = id;
+    this.name = name;
+    this.city = city;
+    this.notes = notes;
+    this.lat = lat;
+    this.lng = lng;
+    this.distance = distance;
+    this.userName = userName;
+    this.picture = picture;
+    this.pictureThumb = pictureThumb;
   }
+}
