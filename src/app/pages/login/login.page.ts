705588c98877ec0a044d455af16f04395afc2523
Wangwei <wangwei0610_outlook.com>
2019-08-19 11:47:26 +0800

    Updated fblogin function

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -2,11 +2,9 @@
 import { Router } from '@angular/router';
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
-import { LoadingController, AlertController } from '@ionic/angular';
+import { AlertController } from '@ionic/angular';
 import { AuthService } from '../../services/user/auth.service';
-import { Plugins } from '@capacitor/core';
-import { FacebookLoginResponse } from '@rdlabo/capacitor-facebook-login';
-import * as firebase from 'firebase/app';
+
 
 @Component({
   selector: 'app-login',
@@ -20,7 +18,6 @@ export class LoginPage implements OnInit {
   public loading: HTMLIonLoadingElement;
 
   constructor(
-    public loadingCtrl: LoadingController,
     public alertCtrl: AlertController,
     private authService: AuthService,
     private router: Router,
@@ -69,39 +66,6 @@ export class LoginPage implements OnInit {
         }
         this.showErrorAlert(errorMessage);
       }
-    }
-  }
-
-  async capacitorFbLogin() {
-    const FACEBOOK_PERMISSIONS = ['public_profile', 'email'];  /* , 'user_friends', 'user_birthday', 'user_photos', 'user_gender' */
-    Plugins.FacebookLogin.login({ permissions: FACEBOOK_PERMISSIONS })
-    .then(async (result: FacebookLoginResponse)  => {
-      if (result.accessToken) {
-        // Login successful.
-        console.log(`Facebook access token is ${result.accessToken.token}`);
-        console.log('Logged into Facebook!', result);
-        this.onFbLoginSuccess(result.accessToken);
-      } else {
-        console.log('fb login error', result);
-        const message = 'facebook login failed';
-        this.showErrorAlert(message);
-      }
-    });
-  }
-
-  async onFbLoginSuccess(accessToken) {
-    const facebookCredential = firebase.auth.FacebookAuthProvider.credential(accessToken.token);
-    const result = await this.authService.loginWithFacebookToken(facebookCredential);
-    console.log('result', result);
-    if ( !result.error ) {
-      this.router.navigateByUrl('');
-    } else {
-      let errorMessage = 'Facebook login failed. Try again later';
-      if ( result.error.message ) {
-        errorMessage = result.error.message;
-      }
-      // error code : "auth/account-exists-with-different-credential"
-      this.showErrorAlert(errorMessage);
     }
   }
 
