c813e5f04cdb5ba49a97eef643adf42e97007e51
Wangwei <wangwei0610_outlook.com>
2019-08-21 23:11:09 +0800

    Added getFriendsAndRecos function for get friends and recommendations data of logged in user

--- src/app/services/friend.service.ts
+++ src/app/services/friend.service.ts
@@ -6,7 +6,6 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
 import 'firebase/storage';
-import { ExplorerService } from './explorer.service';
 
 @Injectable({
   providedIn: 'root'
@@ -17,8 +16,7 @@ export class FriendService {
 
   constructor(
     private storage: Storage,
-    private loadingService: LoadingService,
-    private explorerService: ExplorerService
+    private loadingService: LoadingService
   ) {
     firebase.auth().onAuthStateChanged(user => {
       if (user) {
@@ -39,13 +37,13 @@ export class FriendService {
     const friendsArray: any[] = [];
     // no loading bar
     const userId = firebase.auth().currentUser.uid;
-    await this.loadingService.doFirebaseWithoutLoading( async => {
-        firebase.firestore().collection('userProfile').doc(userId).get().then ( async docUser => {
+    await this.loadingService.doFirebaseWithoutLoading( async () => {
+        await firebase.firestore().collection('userProfile').doc(userId).get().then( async docUser => {
             if ( docUser.data().following ) {
               // resolve(docUser.data().following);
               await Promise.all(docUser.data().following.map(async (friendId) => {
-                  // get friend
-                  const friend = await this.explorerService.getUserByID(friendId);
+                  // get friend details
+                  const friend = await this.getUserByID(friendId);
                   friend.selected = false;
                   friendsArray.push(friend);
                 }));
@@ -55,9 +53,35 @@ export class FriendService {
             return error;
           });
     });
+    console.log('finish get firends');
     return friendsArray;
   }
 
+    // return username and photoURL by user id
+    async getUserByID(userId: string) {
+      return new Promise<any> ((resolve, reject) => {
+        firebase.firestore().collection('userProfile').doc(userId).get().then(docUser => {
+          // if handle is exists then username is handle, else username is email
+          let userName = docUser.data().email;
+          let photoURL = '';
+          if ( docUser.data().handle ) {
+            userName = docUser.data().handle;
+          }
+          if (docUser.data().photoURL) {
+            photoURL = docUser.data().photoURL;
+          }
+          const user = {
+            userId,
+            userName,
+            photoURL
+          };
+          resolve(user);
+        }).catch(error => {
+          console.log('[Get User Info] error = ' + error);
+          reject(error);
+        });
+      });
+    }
 
 
 }
