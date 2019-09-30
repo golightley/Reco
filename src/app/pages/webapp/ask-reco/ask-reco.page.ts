711098f194ad0c9f28bfbeeff1b210b4305e9812
Wangwei <wangwei_0610@outlook.com>
2019-10-01 03:59:19 +0800

    Completed ask-reco function on slide

--- src/app/pages/webapp/ask-reco/ask-reco.page.ts
+++ src/app/pages/webapp/ask-reco/ask-reco.page.ts
@@ -1,7 +1,9 @@
 import { IonSlides } from '@ionic/angular';
 import { AskrecoService } from 'src/app/services/ask-reco.service';
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+
+declare var google;
 
 @Component({
   selector: 'app-ask-reco',
@@ -9,7 +11,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./ask-reco.page.scss'],
 })
 export class AskRecoPage implements OnInit {
-  @ViewChild('recoSlider') slider: IonSlides
+  @ViewChild('recoSlider') slider: IonSlides;
 
   helpString: string;
   signupString: string;
@@ -20,30 +22,54 @@ export class AskRecoPage implements OnInit {
 
   askUserName: string;
   askId: string;
+
+  autocompleteService: any;
+  placeService: any;
+
   constructor(
     private route: ActivatedRoute,
-    private askRecoService: AskrecoService
+    private askRecoService: AskrecoService,
+    private renderer: Renderer2
   ) { }
 
   ngOnInit() {
     this.initialize();
+    this.initGoogleMap();
+  }
+
+  async initialize() {
+    // init asked reco ids
+    this.askRecoService.setAskedRecoId('');
+
+    // init slide
     this.slideIndex = 0;
     this.slideOpts = {
       initialSlide: 0,
       slidesPerView: 1.1
     };
-  }
 
-  async initialize() {
+    // get ask info from parameter
     this.askId = this.route.snapshot.paramMap.get('id');
     console.log('Got parameter => ' + this.askId);
     const askReco = await this.askRecoService.getAskForReco(this.askId);
     console.log(askReco);
     if (askReco) {
-      this.askUserName = 'Liam';
-      this.helpString = 'Help ' + this.askUserName + ' plan his trip to ' + askReco.location;
+      this.askUserName = askReco.userName;
+      this.helpString = `Help ${this.askUserName} plan his trip to ${askReco.location}`;
       this.signupString = 'Signup to save your Recos';
       this.title = this.helpString;
+    }
+  }
+
+  initGoogleMap() {
+    const div = this.renderer.createElement('div');
+    div.id = 'googleDiv';
+    try {
+      this.autocompleteService = new google.maps.places.AutocompleteService();
+      this.placeService = new google.maps.places.PlacesService(div);
+      console.log('Loaded Google AutoComplete service');
+    } catch {
+      console.log('Error to load google service');
     }
   }
 
@@ -51,7 +77,7 @@ export class AskRecoPage implements OnInit {
   setCurIndex() {
     this.slider.getActiveIndex().then(index => {
       this.slideIndex = index;
-    })
+    });
   }
 
 }
--- src/app/pages/webapp/ask-reco/reco-card/reco-card.component.ts
+++ src/app/pages/webapp/ask-reco/reco-card/reco-card.component.ts
@@ -1,5 +1,10 @@
+import { AskrecoService } from './../../../../services/ask-reco.service';
+import { Platform, ToastController } from '@ionic/angular';
 import { Component, OnInit, Input } from '@angular/core';
-import { SafeResourceUrl } from '@angular/platform-browser';
+import { SafeResourceUrl, DomSanitizer } from '@angular/platform-browser';
+import { Plugins, CameraResultType, CameraSource } from '@capacitor/core';
+import { generateThumbImage, getImageSize } from 'src/app/utils/image-utils';
+import { ExplorerService } from 'src/app/services/explorer.service';
 
 @Component({
   selector: 'app-reco-card',
@@ -9,24 +14,30 @@ import { SafeResourceUrl } from '@angular/platform-browser';
 export class RecoCardComponent implements OnInit {
   _cardIndex: number;
   _askUserName: any;
-  addButtonLabel: any;
+  _autocompleteService: any;
+  _placeService: any;
 
-  autocompleteService: any;
-  service: any;
-  placesService: any;
+  addButtonLabel: any;
+  toast: any;
+  isCreatedReco: boolean;
+
+  // place data
+  places: any = [];
   queryPlace: string = '';
   searchDisabled: boolean;
   saveDisabled: boolean;
   location: any;
   googlePlaceId: string;
   googleTypes: any;
-  
+  placePhone: any;
+  placeWebsite: string;
+
+  // image data
   picture: SafeResourceUrl;
-  toast: any;
   originalPicture: string;
   pictureDataUrl; string;
   pictureDataThumbUrl: string;
-  ThumbnailSize = 500;
+  ThumbnailSize = 700;
   PictureSize = 1500;
   limitFileSize = 1024; // KB
 
@@ -37,7 +48,7 @@ export class RecoCardComponent implements OnInit {
   cardTitle = [
     'Your first Reco',
     'Your second Reco',
-    'Save your recos'
+    'Save your Recos'
   ];
 
   @Input()
@@ -48,11 +59,170 @@ export class RecoCardComponent implements OnInit {
   set askUserName(val: any) {
     this._askUserName = val;
   }
-
-  constructor() { }
-
-  ngOnInit() {
-    this.addButtonLabel = 'Add to ' + this._askUserName + '\'s Map';
+  @Input()
+  set autocompleteService(val: any) {
+    this._autocompleteService = val;
+  }
+  @Input()
+  set placeService(val: any) {
+    this._placeService = val;
   }
 
+  constructor(
+    private platform: Platform,
+    private tc: ToastController,
+    private sanitizer: DomSanitizer,
+    private explorerService: ExplorerService,
+    private askRecoService: AskrecoService
+  ) { }
+
+  ngOnInit() {
+    this.addButtonLabel = `Add to ${this._askUserName}'s Map`;
+    this.isCreatedReco = false;
+  }
+
+  searchPlace() {
+    this.saveDisabled = true;
+
+    if (this.queryPlace.length > 0 && !this.searchDisabled) {
+
+      const config = {
+        types: ['establishment'],
+        // types: ['geocode'],
+        input: this.queryPlace
+      };
+
+      this._autocompleteService.getPlacePredictions(config, (predictions, status) => {
+        // console.log('CreateModalPage.SearchPlace.Autocomplete');
+        // console.log(predictions);
+        // console.log(status);
+
+        if (status === 'OK' && predictions) {
+          this.places = [];
+          predictions.forEach((prediction) => {
+            this.places.push(prediction);
+          });
+        }
+
+      });
+
+    } else {
+      this.places = [];
+    }
+
+  }
+
+  selectPlace(place) {
+
+    this.places = [];
+
+    const location = {
+      lat: null,
+      lng: null,
+      name: place.name,
+      city: null
+    };
+
+    this._placeService.getDetails({ placeId: place.place_id }, (details) => {
+      // console.log('CreatePlaceModal.SelectPlace');
+      // console.log(details);
+      // console.log(details.address_components[3].short_name);
+      location.name = details.name;
+      location.lat = details.geometry.location.lat();
+      location.lng = details.geometry.location.lng();
+      location.city = details.address_components[3].short_name;
+      this.saveDisabled = false;
+
+      this.queryPlace = location.name;
+      this.city = location.city;
+      this.googlePlaceId = details.place_id;
+      this.googleTypes = details.types;
+      this.placeWebsite = details.website;
+      this.placePhone = details.international_phone_number;
+
+      this.location = location;
+      console.log(this.location);
+
+    });
+
+  }
+
+  async selectPicture() {
+    console.log(this.platform);
+    if (
+      this.platform.is('hybrid') ||
+      this.platform.is('android') ||
+      this.platform.is('ios')
+    ) {
+      const image = await Plugins.Camera.getPhoto({
+        quality: 100,
+        allowEditing: false,
+        resultType: CameraResultType.DataUrl,
+        source: CameraSource.Photos
+      });
+
+      this.picture = this.sanitizer.bypassSecurityTrustResourceUrl(image && (image.dataUrl));
+      this.originalPicture = image.dataUrl;
+      // console.log('****image data*****');
+      // console.log(this.pictureDataUrl);
+      console.log('****image size*****');
+      console.log(getImageSize(this.originalPicture));
+      // check image file size
+      // if (getImageSize(this.originalPicture) > this.limitFileSize) {
+      // compress image
+      generateThumbImage(this.originalPicture, this.PictureSize, this.PictureSize, 1, data => {
+        this.pictureDataUrl = data;
+        console.log('****compressed image data*****');
+        // console.log(this.pictureDataUrl);
+      });
+      // } else {
+      //   this.pictureDataUrl = this.originalPicture;
+      // }
+      // create a thumbnail
+      generateThumbImage(this.pictureDataUrl, this.ThumbnailSize, this.ThumbnailSize, 1, data => {
+        this.pictureDataThumbUrl = data;
+        console.log('****thumb image data*****');
+        console.log(this.pictureDataThumbUrl);
+      });
+    } else {
+      await this.presentToast('Only available on mobile');
+    }
+  }
+
+  async createAskRec() {
+    const isAskReco = true;
+    const recoID = await this.explorerService.createNewRecommendation(isAskReco,
+      this.queryPlace, this.city, this.notes, this.location, this.googlePlaceId, this.googleTypes,
+      this.placeWebsite, this.placePhone, this.pictureDataUrl, this.pictureDataThumbUrl);
+
+    console.log('Reco ID => ' + recoID);
+    if (recoID) {
+      // if success to create reco
+      await this.presentToast('Successfully saved!');
+      await this.askRecoService.setAskedRecoId(recoID);
+      this.isCreatedReco = true;
+      // const recoIds = await this.askRecoService.getAskedRecoId();
+      // console.log(recoIds);
+
+    }
+
+  }
+
+  async presentToast(message) {
+    const closeText = 'close';
+
+    if (this.toast) {
+      this.toast.dismiss();
+    }
+
+    this.toast = await this.tc.create({
+      message: message,
+      closeButtonText: closeText,
+      showCloseButton: true,
+      duration: 2000,
+      color: 'dark'
+    })
+    this.toast.present();
+  }
+  
 }
